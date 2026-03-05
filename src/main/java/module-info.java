f90f6769008c5aa23469362af48df2c818359303
SuperIce <mail@superice.dev>
2026-03-05 19:28:45 +0800

    build(module): export additional packages for frontend and infrastructure

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,10 @@
 open module gdparser {
     requires org.jetbrains.annotations;
     requires tree_sitter;
+
+    exports dev.superice.gdparser.frontend.ast;
+    exports dev.superice.gdparser.frontend.cst;
+    exports dev.superice.gdparser.infra.treesitter;
+    exports dev.superice.gdparser.frontend.lowering;
+    exports dev.superice.gdparser.frontend.serialize;
 }
