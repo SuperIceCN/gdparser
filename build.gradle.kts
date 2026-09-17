339a5f8c56430a8f72253a9183330e8fd3befddd
SuperIce <mail@superice.dev>
2026-09-18 00:27:49 +0800

    chore(release): bump version to 0.5.4 - Ship a linux aarch64 prebuilt grammar library with existing platform artifacts - Include linux aarch64 in the default native build target set - Extend CI to run tests on linux arm runners

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 val defaultProjectGroup = "dev.superice"
-val defaultProjectVersion = "0.5.3"
+val defaultProjectVersion = "0.5.4"
 val requestedGroup = findProperty("group")?.toString()
 val requestedVersion = findProperty("version")?.toString()
 
